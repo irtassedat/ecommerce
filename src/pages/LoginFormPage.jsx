333ee7d43be8697e7fa69ebd8c9eaeb58a9ea966
irtassedat <sedatirtas.1@gmail.com>
2024-02-27 02:17:08 +0300

    "Refine Login Form UI"

--- src/pages/LoginFormPage.jsx
+++ src/pages/LoginFormPage.jsx
@@ -21,12 +21,13 @@ export default function LoginForm() {
     };
 
     return (
-        <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-teal-500">
-            <div className="w-full max-w-xs">
-                <form onSubmit={handleSubmit(onSubmit)} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="flex items-center justify-center min-h-screen bg-white">
+            <div className="w-full max-w-md">
+                <form onSubmit={handleSubmit(onSubmit)} className="bg-white shadow-lg rounded-lg overflow-hidden p-8">
                     <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email</label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                    <h2 className="text-2xl font-semibold text-center text-gray-800 mb-10">Login</h2>
+                        <label className="block text-gray-700 text-lg font-bold mb-2" htmlFor="email">Email</label>
+                        <input className="shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                             id="email" 
                             type="email" 
                             {...register("email", { required: "Email is required", pattern: /^\S+@\S+\.\S+$/ })}
@@ -35,8 +36,8 @@ export default function LoginForm() {
                     </div>
 
                     <div className="mb-6">
-                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+                        <label className="block text-gray-700 text-lg font-bold mb-2" htmlFor="password">Password</label>
+                        <input className="shadow appearance-none border rounded w-full py-3 px-4 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
                             id="password" 
                             type="password" 
                             {...register("password", { required: "Password is required" })}
