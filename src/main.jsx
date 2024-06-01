aa66fc15b40afd63bae4abd759d0dc3cfec6df81
irtassedat <sedatirtas.1@gmail.com>
2024-06-02 02:01:08 +0300

    testv1 Fix imports and update component structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min.js'
-import  {store}  from './store/store.js';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './store/store.js';
 import { Provider } from 'react-redux';
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-
     <BrowserRouter>
       <App />
     </BrowserRouter>
-
   </Provider>,
-)
+);
