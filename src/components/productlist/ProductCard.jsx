4fd1e62be2d3e9733da16b034c5cac4a5b287e79
irtassedat <sedatirtas.1@gmail.com>
2024-06-12 10:51:11 +0300

    Enhanced appearance of product cards on the shop page.

--- src/components/productlist/ProductCard.jsx
+++ src/components/productlist/ProductCard.jsx
@@ -1,38 +1,39 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { useDispatch } from 'react-redux';
-import { addToCart } from '../../store/actions/shoppingCartAction';
+import { addToCart, addToFavorites } from '../../store/actions/shoppingCartAction';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faCartShopping, faEye, faStar, faMoneyBill } from '@fortawesome/free-solid-svg-icons';
 
-const ProductCard = ({ data, size }) => {
+const ProductCard = ({ data }) => {
     const dispatch = useDispatch();
-    const imageSizeStyle = {
-        width: size[0] + 'px',
-        height: size[1] + 'px',
-    };
-
-    // Ensure data properties are defined
+    
     const productNameSlug = data.name ? data.name.replace(/\s+/g, '-').toLowerCase() : '';
     const productImageUrl = data.images && data.images.length > 0 ? data.images[0].url : '';
 
-    // Sepete ekle butonu için event handler
     const handleAddToCart = () => {
         dispatch(addToCart(data));
     };
 
+    const handleAddToFavorites = () => {
+        dispatch(addToFavorites(data));
+    };
+
     return (
         <div className="max-w-sm mx-auto bg-white dark:bg-zinc-800 rounded-xl shadow-md overflow-hidden md:max-w-2xl transform transition-transform duration-300 hover:scale-105 relative">
-            <div className="relative">
-                <img className="w-full h-48 object-cover" src={productImageUrl} alt={data.name} style={imageSizeStyle} />
+            <div className="relative aspect-w-1 aspect-h-1">
+                <img className="w-full h-full object-cover" src={productImageUrl} alt={data.name} />
                 <span className="absolute top-2 left-2 bg-red-500 text-white text-xs font-semibold px-2 py-1 rounded">Sale</span>
                 <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex space-x-2">
                     <button className="bg-white dark:bg-zinc-700 p-2 rounded-full shadow-md opacity-75 hover:opacity-100" onClick={handleAddToCart}>
-                        <FontAwesomeIcon icon={faHeart} />
+                        <FontAwesomeIcon icon={faCartShopping} />
                     </button>
                     <Link to={`/${data.category_id}/${data.id}/${productNameSlug}`} className="bg-white dark:bg-zinc-700 p-2 rounded-full shadow-md opacity-75 hover:opacity-100">
                         <FontAwesomeIcon icon={faEye} />
                     </Link>
+                    <button className="bg-white dark:bg-zinc-700 p-2 rounded-full shadow-md opacity-75 hover:opacity-100" onClick={handleAddToFavorites}>
+                        <FontAwesomeIcon icon={faHeart} />
+                    </button>
                 </div>
             </div>
             <div className="p-4">
