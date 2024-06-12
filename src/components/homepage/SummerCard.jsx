7e7f53d7f9d4be06ef13ec93ff5eba847ef8398a
irtassedat <sedatirtas.1@gmail.com>
2024-06-12 10:52:43 +0300

    Redirected the initial structure of the project towards relevant categories.

--- src/components/homepage/SummerCard.jsx
+++ src/components/homepage/SummerCard.jsx
@@ -1,5 +1,7 @@
-export default function SummerCard() {
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
+export default function SummerCard() {
   return (
       <section className="w-screen">
           <div className="my-0 mx-auto max-w-[1050px] flex justify-between items-center gap-20">
@@ -11,8 +13,12 @@ export default function SummerCard() {
                   <h2 className="font-bold text-[40px] leading-[50px]">Part of the Neural Universe</h2>
                   <p className="text-xl leading-[30px]">We know how large objects will act, but things on a small scale.</p>
                   <div className="font-bold text-sm leading-[22px] flex gap-2.5">
-                      <button className="bg-[#2DC071] text-white px-10 py-4 rounded-md border-2 border-[#2DC071] border-solid">BUY NOW</button>
-                      <button className="bg-white text-[#2DC071] px-10 py-4 rounded-md border-2 border-[#2DC071] border-solid">READ MORE</button>
+                      <NavLink to="/shop">
+                        <button className="bg-[#2DC071] text-white px-10 py-4 rounded-md border-2 border-[#2DC071] border-solid">BUY NOW</button>
+                      </NavLink>
+                      <NavLink to="/shop">
+                        <button className="bg-white text-[#2DC071] px-10 py-4 rounded-md border-2 border-[#2DC071] border-solid">READ MORE</button>
+                      </NavLink>
                   </div>
               </div>
           </div>
